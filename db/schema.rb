cb75e2add13e2bcd4885d9081b81265e5f39b04c
Greta Didziokaite <gretadidziokaite@yahoo.ie>
2018-08-07 15:31:50 +0100

    create reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807125610) do
+ActiveRecord::Schema.define(version: 20180807140647) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_reviews_on_city_id"
   end
 
 end
