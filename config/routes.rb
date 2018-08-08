234ee4b47e33695ee958d754ceec6edda7c948e3
Greta Didziokaite <gretadidziokaite@yahoo.ie>
2018-08-08 15:56:48 +0100

    add some styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :reviews
   resources :cities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root 'static_pages#home'
+  end
