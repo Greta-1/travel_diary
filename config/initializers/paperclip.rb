6b0eed2717129a01c6b73ee0cdfd18899d5bfcba
Greta Didziokaite <gretadidziokaite@yahoo.ie>
2018-08-10 13:05:19 +0100

    update paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 'teen-turn-travel-diary.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
